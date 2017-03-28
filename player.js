08f83e7f21517318226533254f0da7a5b8109903
vartovi <viljo.varto@gmail.com>
2017-03-28 13:47:57 +0300

    First working version

---
+++ player.js
@@ -0,0 +1,8 @@
+var player = {
+    x: 1,
+    y: 60,
+    width: 20,
+    height: 20
+};
+
+
---
+++ scripts.js
@@ -0,0 +1,73 @@
+var canvas = document.getElementById("gameDisplay");
+var ctx = canvas.getContext("2d");
+var walls = [];
+
+function loadGame() {
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+function startGame() {
+    var frameCount = 0;
+    var interval = setInterval(function(){
+        ctx.fillStyle = "black";
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        player.y += 0.5;
+        ctx.fillRect(player.x, player.y, 20, 20);
+        $("#points").html("Points: " +frameCount);
+        for (var i = 0; i < walls.length; i++){
+            walls[i].x -= 2;
+            if(walls[i].x < -100){
+                walls.splice(i,1);
+            }
+            ctx.fillRect(walls[i].x, walls[i].y, 10, 100);
+            ctx.fillRect(walls[i].x, (walls[i].y - canvas.height + 10), 10, 100);
+            if((walls[i].x <= 20 && walls[i].x >= 1) && (walls[i].y <= (player.y+20) || (walls[i].y-40) >= player.y)){
+                clearInterval(interval);
+                gameOver();
+            }
+        }
+        if ((frameCount % 40) == 0){
+            var random = Math.floor((Math.random() * 100) + 40);
+            walls.push(new obstacle(300, random));
+        }
+        frameCount++;
+    }, 50);
+}
+
+function gameOver() {
+    walls = [];
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "Red";
+    ctx.fillText("Game over",70,70);
+}
+
+function moveUp() {
+    player.y -= 5;
+}
+
+function moveDown() {
+    player.y += 2;
+}
+
+var obstacle =  function (x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 5;
+    this.height = 110;
+};
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 38: // up
+            moveUp();
+            break;
+
+        case 40: // down
+            moveDown();
+            break;
+
+        default: return;
+    }
+    e.preventDefault();
+});
